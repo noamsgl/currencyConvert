78dcaff78c3df8cbefe098e7f872dc64c59dd6ad
noam <noamsi@post.bgu.ac.il>
2020-08-13 11:36:19 +0300

    Added comments

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -8,18 +8,18 @@ public class Main {
     public static void main(String[] args) {
         String fname = args[0];
 
+        //Read data from input file
         fileHandler fhandler = new fileHandlerImpl(fname);
-
         String baseCurrency = fhandler.getFromCurrency();
         String toCur = fhandler.getToCurrency();
         List<Double> values = fhandler.getValues();
 
+        //Get exchange rate from API
         ExchangeRateAPI api = new ExchangeRateAPIImpl(baseCurrency);
         Double exchangeRate = api.getExchangeRate(toCur);
 
+        //Print converted values to System.out
         outputHandler outPutHandler = new outputHandlerImpl(values, exchangeRate);
-
         outPutHandler.print();
-
     }
 }
